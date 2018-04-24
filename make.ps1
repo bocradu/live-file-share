c672762ed00b0f1d88b29b5992372f18cf4c9a57
Boc <radu.boc@haufe-lexware.com>
2018-04-24 10:21:48 +0300

    removed token

--- make.ps1
+++ make.ps1
@@ -1,13 +1,21 @@
+Write-Output 'Packaging app'
 npm run package
+
+Write-Output 'Generating zip file'
 Compress-Archive -Path .\out\live-file-share-win32-x64 -DestinationPath .\out\dist\live-file-share-win32-x64.zip -Force
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add('Authorization', 'token c5766b72731dc699ed737ede036f02087ac165e3')
+$token = 'token ' + $Env:GITHUB_TOKEN
+$headers.Add('Authorization', $token)
 $uri = "https://api.github.com/repos/bocradu/live-file-share/releases"
 $body = '{"tag_name": "v1.0.5",  "target_commitish": "master","name": "Live-file-share.v1.0.5","body": "","draft": false,"prerelease": false}'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Write-Output 'Creating release'
 $content = (Invoke-WebRequest -UseBasicParsing $uri -ContentType "application/json" -Method POST -Body $body -Headers $headers).Content | ConvertFrom-Json
 $headers.Add('Content-Type', "application/zip")
 $uploadUrl = $content.upload_url.Split([char]0x007B)[0] + "?name=live-file-share-win32-x64.zip"
+
+Write-Output 'Upload zip file'
 Invoke-WebRequest -UseBasicParsing $uploadUrl -ContentType "application/json" -Method POST -InFile .\out\dist\live-file-share-win32-x64.zip -Headers $headers
 
 
